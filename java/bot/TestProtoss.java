c998350678ddeea0ccc6d0c3492e0bda0c028f35
rj-90 <ra0633a@american.edu>
2016-11-22 01:45:23 -0500

    adding and testing git

---
+++ java/bot/TestProtoss.java
@@ -0,0 +1,52 @@
+package bot;
+
+import jnibwapi.BWAPIEventListener;
+import jnibwapi.Position;
+
+/**
+ * Created by James Raynor on 11/22/16.
+ */
+public class TestProtoss implements BWAPIEventListener {
+
+    public void matchStart() {}
+    public void matchFrame(){}
+    public void connected(){}
+
+
+    @Override
+    public void keyPressed(int keyCode) {}
+    @Override
+    public void matchEnd(boolean winner) {}
+    @Override
+    public void sendText(String text) {}
+    @Override
+    public void receiveText(String text) {}
+    @Override
+    public void nukeDetect(Position p) {}
+    @Override
+    public void nukeDetect() {}
+    @Override
+    public void playerLeft(int playerID) {}
+    @Override
+    public void unitCreate(int unitID) {}
+    @Override
+    public void unitDestroy(int unitID) {}
+    @Override
+    public void unitDiscover(int unitID) {}
+    @Override
+    public void unitEvade(int unitID) {}
+    @Override
+    public void unitHide(int unitID) {}
+    @Override
+    public void unitMorph(int unitID) {}
+    @Override
+    public void unitShow(int unitID) {}
+    @Override
+    public void unitRenegade(int unitID) {}
+    @Override
+    public void saveGame(String gameName) {}
+    @Override
+    public void unitComplete(int unitID) {}
+    @Override
+    public void playerDropped(int playerID) {}
+}
